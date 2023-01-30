696b50a9eb4d2d2a3875ce7686c761c19589ab71
unknown <patryk.pora@shitcoins.club>
2023-01-30 20:35:51 +0100

    h2 database to file ddl-auto create

--- src/main/java/pl/patrykpora/repository/PlayerRepository.java
+++ src/main/java/pl/patrykpora/repository/PlayerRepository.java
@@ -4,6 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.patrykpora.database.entities.PlayerEntity;
 
-@Repository
 public interface PlayerRepository extends JpaRepository<PlayerEntity, Long> {
 }
