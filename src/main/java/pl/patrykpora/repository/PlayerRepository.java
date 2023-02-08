03534a6b235ad52ccd6fdc7d734b921485c22e96
unknown <patryk.pora@shitcoins.club>
2023-02-08 20:15:56 +0100

    template for front first controller

--- src/main/java/pl/patrykpora/repository/PlayerRepository.java
+++ src/main/java/pl/patrykpora/repository/PlayerRepository.java
@@ -1,7 +1,6 @@
 package pl.patrykpora.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import pl.patrykpora.database.entities.PlayerEntity;
 
 public interface PlayerRepository extends JpaRepository<PlayerEntity, Long> {
